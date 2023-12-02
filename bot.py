84a94455322022ad816ef916d7e9728801638b4a
sergeyparshi <tannkk321@gmail.com>
2023-12-02 10:13:26 +0300

    добавил SQL

--- bot.py
+++ bot.py
@@ -5,7 +5,17 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram.types import ReplyKeyboardMarkup, InputFile, InlineKeyboardMarkup, InlineKeyboardButton
 from dotenv import load_dotenv
 import os
+import sqlite3 as sq
 
+with sq.connect('users.db') as con:#создание бд
+    cur = con.cursor() #курсор
+    cur.execute(""" TABLE users (
+    id INTEGER,
+    chatid INTEGER,
+    name TEXT,
+    surname TEXT,
+    law TEXT
+    )""")
 load_dotenv()
 bot = Bot(os.getenv('TOKEN'))
 group_id = os.getenv('GROUP_ID')
