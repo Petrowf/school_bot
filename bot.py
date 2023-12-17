5827252ab3748418b7c7ac9b08846380b0c7fd30
sergeyparshi <tannkk321@gmail.com>
2023-12-17 13:16:29 +0700

    удалил создание бд из bot.py

--- bot.py
+++ bot.py
@@ -8,15 +8,6 @@ from dotenv import load_dotenv
 import os
 import sqlite3 as sq
 
-with sq.connect('users.db') as con:#создание бд
-    cur = con.cursor() #курсор
-    cur.execute(""" TABLE users (
-    id INTEGER,
-    chatid INTEGER,
-    name TEXT,
-    surname TEXT,
-    law TEXT
-    )""")
 load_dotenv()
 bot = Bot(os.getenv('TOKEN'))
 group_id = os.getenv('GROUP_ID')
