dc01082d773095dbc6e023890c2a7e8c50fa4dd6
sergeyparshi <tannkk321@gmail.com>
2023-12-17 13:50:28 +0700

    доделал проверку человека в бд и в случчае отсутсвия его, добавил добавление данных

--- sql_inj.py
+++ sql_inj.py
@@ -1,10 +1,10 @@
 import sqlite3 as sq
 
-def proverka_prav(user, cur, con, first_name):
+def proverka_prav(user, cur, con, first_name, last_name, chat_id, access):
     query = f'SELECT name FROM users WHERE id={id}'
     us_name = cur.fetchone()
     if us_name == None:
-        f'INSERT INTO users VALUES({id,})'
+        f'INSERT INTO users VALUES({id, chat_id, us_name, first_name, last_name, access})'
 
 
 if __name__ == "__main__":
