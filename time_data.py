55bb75a4f517b8aef485a8241bf5021f05fd44e2
v-progs <nononame460@gmail.com>
2024-02-10 15:24:13 +0700

    Бета

---
+++ time_data.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# Function to get the subjects and times by class
+
+
+
+# Example usage: Get the subjects and times for Class  8A
+timetable = get_subjects_and_times_by_class(df, "Class  8A")
+print(timetable)
